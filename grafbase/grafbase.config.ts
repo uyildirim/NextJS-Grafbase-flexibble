837cfa491570755563db8a6f4118ddb0ec790eed
uyildirim <uguryildirim1903@gmail.com>
2023-11-19 00:25:53 +0300

    .

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,6 @@
 import { g, config } from "@grafbase/sdk";
 
-const User = g.model("USer", {
+const User = g.model("User", {
   name: g.string().length({ min: 2, max: 20 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
