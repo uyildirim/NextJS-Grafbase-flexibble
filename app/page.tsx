1e876fa0cb7d4d2e8eedbb3b96b482bbe25a7e3c
uyildirim <uguryildirim1903@gmail.com>
2023-11-19 00:10:02 +0300

    grafbase move folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <section className="flex-start flex-col paddings mb-169">Category</section>
+    <section className="flex-start flex-col paddings mb-169">Category </section>
   );
 }
